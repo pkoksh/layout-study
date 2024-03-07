39534f6748f379b9424b13054fab617c4808d389
shkim <pkoksh@gmail.com>
2024-03-07 22:02:59 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Layout, Model } from "flexlayout-react";
+import "flexlayout-react/style/light.css";
+import Sample1 from "./component/sample1";
+var json = {
+  global: {
+    splitterSize: 40,
+    tabSetEnableClose: true,
+    tabSetHeaderHeight: 100,
+    // tabSetTabStripHeight: 1,
+  },
+  borders: [],
+  layout: {
+    type: "row",
+    weight: 100,
+    children: [
+      {
+        type: "tabset",
+        weight: 20,
+        children: [
+          {
+            type: "tab",
+            name: "One",
+            component: "button",
+          },
+        ],
+      },
+      {
+        type: "tabset",
+        weight: 80,
+        children: [
+          {
+            type: "tab",
+            name: "Two",
+            component: "button",
+          },
+          {
+            type: "tab",
+            name: "Three",
+            component: "sample1",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const model = Model.fromJson(json);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const factory = (node) => {
+    var component = node.getComponent();
+
+    if (component === "button") {
+      return <button>{node.getName()}</button>;
+    } else if (component === "sample1") {
+      return <Sample1></Sample1>;
+    }
+  };
+
+  return <Layout model={model} factory={factory} />;
 }
 
 export default App;
